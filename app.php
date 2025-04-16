091e2667432b508bfb6f2a0ad118113308a79f4d
Nicolas Linares <125098430+veganico98@users.noreply.github.com>
2025-04-15 21:02:41 -0300

    Add files via upload

---
+++ app.php
@@ -0,0 +1,105 @@
+<?php
+//clase dashboard
+class dashboard{
+    public $data_inicio;
+    public $data_fim;
+    public $numeroVendas;
+    public $totalVendas;
+
+    public function __get($atrr) {
+        return $this->$atrr;
+    }
+
+    public function __set($atrr, $value){
+        $this->$atrr = $value;
+        return $this;
+    }
+}
+
+//Conexão
+class Conexao{
+    private $host = 'localhost';
+    private $dbname = 'dashboard';
+    private $user = 'root';
+    private $pass = '';
+
+    public function conectar() {
+        try{
+
+            $conexao = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname","$this->user",
+                "$this->pass"
+            );
+            
+            //utf8
+            $conexao->exec('set charset utf8');
+
+            return $conexao;
+
+        } catch (PDOException $e){
+            echo '<p>'.$e->getMessage().'</p>';
+        }
+    }
+}
+
+//classe (model)
+class Bd {
+    private $conexao;
+    private $dashboard;
+
+    public function __construct(Conexao $conexao, Dashboard $dashboard){
+        $this->conexao = $conexao->conectar();
+        $this->dashboard = $dashboard;
+    }
+
+    public function getNumeroVendas() {
+        $query = '
+            select 
+                count(*) as numero_vendas 
+            from 
+                tb_vendas 
+            where 
+                data_venda between :data_inicio and :data_fim';
+
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
+    }
+
+    public function getTotalVendas() {
+        $query = '
+            select 
+                SUM(total) as total_vendas 
+            from 
+                tb_vendas 
+            where 
+                data_venda between :data_inicio and :data_fim';
+
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ)->total_vendas;
+    }
+}
+
+//Lógica Script
+$dashboard = new Dashboard();
+
+$conexao = new Conexao();
+
+$dashboard->__set('data_inicio', '2018-08-01');
+$dashboard->__set('data_fim', '2018-08-31');
+
+$bd = new Bd($conexao, $dashboard);
+
+$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
+$dashboard->__set('total_vendas', $bd->getTotalVendas());
+
+print_r($dashboard);
+
+?>
---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+	$('#documentacao').on('click', () => {
+        // $('#pagina').load('documentacao.html')
+        
+        // $.get('documentacao.html', data => {
+        //     $('#pagina').html(data)
+        // })
+
+        $.post('documentacao.html', data => {
+            $('#pagina').html(data)
+        })
+    })
+    $('#suporte').on('click', () => {
+        // $('#pagina').load('suporte.html')
+        
+        // $.get('suporte.html', data => {
+        //     $('#pagina').html(data)
+        // })
+
+        $.post('suporte.html', data => {
+            $('#pagina').html(data)
+        })
+    })
+})
